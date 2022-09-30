034bb450cfe6ea69ce9520172df164686584c088
Дмитрий Куулар <kuulardmo@bk.ru>
2022-09-30 14:17:35 +0700

    Added tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,45 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldShowNecessarySum() {
+        int expected = 200;
+        int actual = service.remain(800);
+        assertEquals(actual, expected);
+    }
+
+//    @Test
+//    public void shouldShowNullIfAccurateOneThousand() {
+//        int expected = 0;
+//        int actual = service.remain(1000);
+//        assertEquals(actual, expected);
+//    }
+
+    @Test
+    public void shouldShowAllSum() {
+        int expected = 1000;
+        int actual = service.remain(0);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldShowNecessarySumIfOneRuble() {
+        int expected = 999;
+        int actual = service.remain(1);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldShowNecSumIfMoreThanThousand() {
+        int expected = 1;
+        int actual = service.remain(1999);
+        assertEquals(actual, expected);
+    }
+}
